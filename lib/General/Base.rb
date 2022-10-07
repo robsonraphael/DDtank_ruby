05341ffc6b8d577529ea1203c06d647fec70dd7c
Robson Raphael <robsonraphael2001@gmail.com>
2022-10-07 00:19:59 -0300

    Create metodos

--- lib/General/Base.rb
+++ lib/General/Base.rb
@@ -1,103 +1,103 @@
-# Module Base DDtank
+# Model Base DDTank
 module Base
-  # Class Basic Atributes
+  # Class Basic Attributes
   class BasicAttributes
-    attr_reader :attack, :defense, :agility, :luck
-
     def initialize(*_args)
-      # Atribute Base
-      @attack = 1
-      @defense = 1
-      @agility = 1
-      @luck = 1
+      @attributes = {
+        attack: (@attack = 1), defense: (@defense = 1),
+        agility: (@agility = 1), luck: (@luck = 1)
+      }
     end
 
-    def level_up(array = @level)
-      @attack += 2 + array[0]
-      @defense += 4 + array[1]
-      @agility += 3 + array[2]
-      @luck += 1 + array[3]
+    # Mostrar Atributos
+    def showing_attributes
+      puts '======================='
+      @attributes.each_pair { |key, value| puts "#{key} => #{value}" }
+      puts '======================='
     end
-    private :level_up
+    private :showing_attributes
+
+    def update_attributes
+      @attributes[:attack] += (2 + @level)
+      @attributes[:defense] += (4 + @level)
+      @attributes[:agility] += (3 + @level)
+      @attributes[:luck] += (1 + @level)
+    end
+    private :update_attributes
   end
 
   # Class Atributes
   class Attributes < BasicAttributes
-    attr_reader :damage, :armo, :hp, :strong, :total_force
-
     def initialize(*args)
       super
-      # Atribute Force
-      @damage = 1
-      @armo = 1
-      @hp = 900
-      @hp_max = 900
-      @strong = 1
+      @attributes[:damage] = (@damage = 1)
+      @attributes[:armo] = (@armo = 1)
+      @attributes[:hp] = (@hp = 1)
+      @attributes[:hp_max] = (@hp_max = 900)
+      @attributes[:strong] = (@strong = 1)
+
       # Force Total
       @total_force = 100
     end
 
-    def level_up(*args)
-      return false if @level >= 45
-
-      @level += 1
-
+    def update_attributes
       super
-      @damage = (@luck * @attack) - @defense / 100
-      @armo = @defense * @luck
-      @hp_max += @level * luck
-      @strong = @defense + (@agility * @level)
+      @attributes[:damage] += (@luck * @attack)
+      @attributes[:armo] += (@defense * @luck)
+      @attributes[:hp_max] += (@level * @luck)
+      @attributes[:strong] += (@defense + (@agility * @level))
     end
+    private :update_attributes
   end
 
   # Class Equipaments
   class Equipaments < BasicAttributes
-    attr_reader :name, :type, :genre
-
-    def initialize(array)
-      @name = array.first
-      @type = array[1]
+    def initialize(hash)
+      @name = hash[:name]
+      @type = hash[:type]
+      @genre = hash[:genre] unless hash[:genre].nil?
       @level = 0
       super
     end
-  end
 
-  # Class Player
-  class Character < Attributes
-    def initialize(name, genre, type_class)
-      # Core
-      @name = name.to_s
-      @genre = genre.upcase.freeze
-      @class = type_class.upcase.freeze
-      @level = 1
-      @exp = 0
-      @exp_max = 10
-      # Atributes
-      super
+    def level_up
+      return false if @level >= 12
+
+      @level += 1
+      update_attributes
     end
 
     def info
       puts <<~INFO
-        ========================
-        Nome do personagem: #{@name}
-        Gênero: #{@genre}
-        Classe: #{@class}
-        Nível: #{@level}
-
-        ========================
-            A T R I B U T O S
-        ========================
-
-        ATAQUE : #{@attack}
-        DEFESA : #{@defense}
-        AGILIDADE : #{@agility}
-        SORTE : #{@luck}
-
-        DANO : #{@damage}
-        ARMADURA: #{@armo}
-        VIDA : #{@hp_max}
-        FORÇA : #{@strong}
+        =======================
+          E Q U I P A M E N T
+        =======================
+        NAME: #{@name}
+        TIPO: #{@type}
+        GENRE: #{@genre}
+        LEVEL: #{@level}
       INFO
+      showing_attributes
+    end
+  end
+
+  # Class Characte
+  class Character < Attributes
+    def initialize(hash)
+      # Core
+      @name = hash[:name]
+      @genre = hash[:genre]
+      @class = hash[:class]
+      @level = 1
+      # Atributes
+      super
+    end
+
+    def level_up
+      return false if @level >= 45
+
+      @level += 1
+      update_attributes
     end
   end
 end
